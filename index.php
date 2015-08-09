f4c99b540841db84f4d60be18780693fa1e52046
Michael Mahler <mmahler2@gmail.com>
2015-08-08 22:59:02 -0500

    Refactored a bit, added some comments.

--- index.php
+++ index.php
@@ -2,29 +2,40 @@
 
 require_once('config.php');
 
+
+
 function redirect($uri) {
+	//clean up the URL a bit- removes trailing slashes
 	$uri_hash = str_replace('/'.SUB_URI,'',$uri);
+	
+	//connect to the database
 	$cn = new mysqli(DB_HOST, DB_UN, DB_PW, DB_NAME);
 	if ($cn->connect_error)
 		die("Connection failed: " . $cn->connect_error);
+	
+	//form our query
 	$q = "select redirect,custom from ".DB_TABLE." where hash = '$uri_hash'" ;
 	$res = $cn->query($q);
 	while($row = $res->fetch_assoc()) {
+		//extra bonus! analytics!
 		if ($row['custom'] == 1) {
 			$sql = "UPDATE ".DB_TABLE." SET hits=hits+1 WHERE hash = '$uri_hash'";
 			$cn->query($sql);
 		}
+		//and they're out of here!
 		header("Location: ".$row['redirect']);
 	}	
 }
 
 
-
+//determine where we are.
 $uri = $_SERVER['REQUEST_URI'];
 if ($uri == "/".SUB_URI) {
+	//if the current URL matches the install path, show them the body.
 	include('body.php');
 }
 else {
+	//otherwise, redirect them since that's what they want.
 	redirect($uri);
 }
 
--- new.php
+++ new.php
@@ -2,41 +2,31 @@
 
 require_once('config.php');
 
-
-
+//connect to the database
 $cn = new mysqli(DB_HOST, DB_UN, DB_PW, DB_NAME);
 if ($cn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 
-
-
 function addNew($hash, $url, $isCustom) {
 	global $cn;
-	
 	$q = "INSERT INTO  `".DB_TABLE."` (`hash`,`redirect`,`custom`,`hits`) VALUES ('$hash','$url','$isCustom',0)";
-	if ($cn->query($q)) {
+	if ($cn->query($q))
 		echo("<h3>Your new <span class='tiny'>(smahl)</span> URL is: <a href='".SUB_URI."$hash'>".SUB_URI."$hash</a></h3>");
-	}
-	else {
+	else
 		echo("FAILURE");
-	}
-	
 }
 
 function reHash($hash, $url){
 	global $cn;
-	
 	$hash = substr(hash('adler32', $hash, false), 0, 5); 
 	$q = "select * from ".DB_TABLE." where hash = '$hash'" ;
 	$res = $cn->query($q);
-	if ($res->num_rows > 0){
+	if ($res->num_rows > 0)
 		rehash($hash, $url);
-	}
 	else
 		addNew($hash, $url);
 }
-
 
 function cleanInput($input) {
 	$search = array(
@@ -60,20 +50,17 @@ function go() {
 	$custom = cleanInput($_POST['custom']);
 	$isCustom = (strlen($custom) > 0) ? 1 : 0;
 	
-	
 	//not tiny enough.
 	if (strlen($custom) >= 15) {
 		echo('<h3>Custom URL too long! (Max 14 chars)</h3>');
 		die();
 	}
-	
 
 	//is custom or not?
 	if ($isCustom)
 		$hash = $custom;
 	else
 		$hash = substr(hash('adler32', $url, false), 2, 4); 
-	
 	
 	//if the hash has been used, return the url it belongs to
 	$q = "select redirect from ".DB_TABLE." where hash = '$hash'" ;
@@ -82,16 +69,10 @@ function go() {
 		$checkURL = $row['redirect'];
 	}
 	
-	
 	//url was already used, but wasn't custom
-	if (($checkURL == $url) && ($url !== "") && $check == 1 && $isCustom == 0) {
-		$check = 0;
-		echo("<h3>Your new <span class='tiny'>(smahl)</span> URL is: <a class='tinyurl' href='".SUB_URI."$hash'>".SUB_URI."$hash</a></h3>");
-		die();
-	}
-	
+	//OR
 	//custom url already used, but it was the the same redirect
-	if (($checkURL == $url) && ($url !== "") && $check == 1 && $isCustom == 1) {
+	if (($checkURL == $url) && ($url !== "") && $check == 1) {
 		$check = 0;
 		echo("<h3>Your new <span class='tiny'>(smahl)</span> URL is: <a class='tinyurl' href='".SUB_URI."$hash'>".SUB_URI."$hash</a></h3>");
 		die();
@@ -104,29 +85,15 @@ function go() {
 		die();
 	}
 	
-	
-	
-	
-	
 	if($res->num_rows > 0 && $check == 1 && $isCustom == 0){
 		reHash($hash, $url);
 	}
 	else if (($url !== "") && ($check == 1)){
 		addNew($hash, $url, $isCustom );
 	}
-	else if ($check == 1){
-		echo("<h3 class='error'>You Missed Something....&uarr;</h3>");
-		die();
-	}
-	
 }
 
-
-
-
-
 go();
-
 
 
 ?>
